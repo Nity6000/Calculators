d466c4567e0e8af54e44d18564fd48f8edeffb96
Nity6000 <31748962+Nity6000@users.noreply.github.com>
2018-02-17 14:38:38 -0500

    Fixed

--- C++/main.cpp
+++ C++/main.cpp
@@ -10,9 +10,23 @@ int main()
 
     cout << "Enter your first number" << endl;
     cin >> firstNumber;
+    if (cin.fail())
+    {
+        firstNumber = 0;
+        cout << "Thats not a number, so we're going to make it 0" << endl;
+        std::cin.clear();
+        std::cin.ignore(256,'\n');
+    }
 
     cout << "Enter your second number" << endl;
     cin >> secondNumber;
+    if (cin.fail())
+    {
+        secondNumber = 0;
+        cout << "Thats not a number, so we're going to make it 0" << endl;
+        std::cin.clear();
+        std::cin.ignore(256,'\n');
+    }
 
     cout << "Enter the operation you want to do" << endl;
     cin >> operation;
