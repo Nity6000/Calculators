6291a8f0bf0962bf3e6b9be82bd2e8a5c738a1a5
Nity6000 <31748962+Nity6000@users.noreply.github.com>
2018-02-17 10:46:24 -0500

    Added C++

---
+++ C++/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float firstNumber;
+    float secondNumber;
+
+    char operation;
+
+    cout << "Enter your first number" << endl;
+    cin >> firstNumber;
+
+    cout << "Enter your second number" << endl;
+    cin >> secondNumber;
+
+    cout << "Enter the operation you want to do" << endl;
+    cin >> operation;
+
+    switch (operation)
+    {
+        case '+':
+            cout << firstNumber + secondNumber << endl;
+            break;
+        case '-':
+            cout << firstNumber - secondNumber << endl;
+            break;
+        case '*':
+            cout << firstNumber * secondNumber << endl;
+            break;
+        case '/':
+            cout << firstNumber / secondNumber << endl;
+            break;
+        default:
+            cout << "Invalid Operation, use either +, -, *, or /" << endl;
+            break;
+    }
+}
